16ed6fd42f2850a96008f17fd0871f8e620a1335
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-17 16:39:41 -0300

    Exercicio 009 - Label
    Justificação do texto: RIGHT, LEFT, CENTER

--- 009.py
+++ 009.py
@@ -9,7 +9,7 @@ janela.geometry("500x500")
 # Justificação do texto: RIGHT, LEFT, CENTER
 label_01 = Label(
     janela,
-    text="frase de teste frase de teste frase de \nteste frase de teste frase de teste\nfrase de teste frase de teste",
+    text="frase de teste frase de teste frase de\nfrase de teste frase de teste",
     font="Arial 20",
     width=30,
     height=10,
@@ -18,7 +18,6 @@ label_01 = Label(
     justify=LEFT,
     anchor=E,
 )
-label_01.pack()
-
+label_01.place(x=100, y=20)
 
 janela.mainloop()
