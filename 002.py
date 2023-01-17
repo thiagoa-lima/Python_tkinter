d8cf5acdd8286edcea4fb2009f4ca3c89254c237
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-17 14:30:00 -0300

    pequenos ajustes

--- 002.py
+++ 002.py
@@ -3,14 +3,12 @@ from tkinter import *
 menu_inicial = Tk()
 
 menu_inicial.title("Título")
-# menu_inicial["bg"] = "gray"
+menu_inicial["bg"] = "gray"
 menu_inicial.geometry("500x300+400+250")
-
 
 # botão
 def btn_Click(mensagem):
     print(mensagem)
-
 
 btn_01 = Button(
     menu_inicial,
